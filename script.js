c08eb1f4a88ea0e7e6b37b21e216c9170629aa13
th-nhan <tthhaannnnhhaann@gmail.com>
2025-02-06 14:05:16 +0700

    finish

---
+++ script.js
@@ -0,0 +1,52 @@
+const gameContainer = document.querySelector(".container");
+const userResult = document.querySelector(".user_result img");
+const botResult = document.querySelector(".bot_result img");
+const result = document.querySelector(".result");
+const optionImages = document.querySelectorAll(".option_image");
+
+const botImages = ["photo/rock.png","photo/paper.png","photo/scissors.png"];
+const outcomes = {
+    RR: "DRAW",
+    RP: "BOT",
+    RS: "YOU",
+    PP: "DRAW",
+    PR: "YOU",
+    PS: "BOT",
+    SS: "DRAW",
+    SR: "BOT",
+    SP: "YOU",
+};
+
+function handleOptionClick(event){
+    const clickedImage = event.currentTarget;
+    const clickedIndex = Array.from(optionImages).indexOf(clickedImage);
+    userResult.src = botResult.src = "photo/rock.png";
+    result.textContent = "Wait...";
+    optionImages.forEach((image,index) =>{
+        image.classList.toggle("active",index === clickedIndex);
+    });
+
+    gameContainer.classList.add("start");
+    setTimeout(()=>{
+        gameContainer.classList.remove("start");
+
+        const userImageSrc = clickedImage.querySelector("img").src;
+        userResult.src = userImageSrc;
+
+        const randomNumber = Math.floor(Math.random() * botImages.length);
+
+        const botImageSrc =  botImages[randomNumber];
+        botResult.src = botImageSrc;
+
+        const userValue = ["R","P","S"][clickedIndex];
+        const botValue = ["R","P","S"][randomNumber];
+        const outcomeKey = userValue + botValue;
+        const outcome = outcomes[outcomeKey] || "Unknown";
+        result.textContent = userValue === botValue ? "Match Draw":`${outcome} WON!`
+
+    },2500);
+}
+
+optionImages.forEach(image => {
+    image.addEventListener("click", handleOptionClick);
+});
